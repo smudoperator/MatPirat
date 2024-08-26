825ccd389b026eb40e3503cad23aa55f3dddcb32
smudoperator <simon.halland99@gmail.com>
2024-08-26 14:05:47 +0200

    changes

--- deploy.cmd
+++ deploy.cmd
@@ -1,15 +1,16 @@
 @echo off
-:: 1. Create app_offline.htm to stop the app during deployment
-echo Creating app_offline.htm
-echo app_offline.htm > "%DEPLOYMENT_TARGET%\app_offline.htm"
+setlocal
 
-:: 2. KuduSync to sync files, but exclude the database file
-echo Syncing files, excluding dinners.db
-:: Use KuduSync with the --exclude option
-KuduSync.NET.exe -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n --exclude="dinners.db"
+REM Set variables
+set ZIP_PATH=%DEPLOYMENT_TARGET%\extracted
+set TARGET_PATH=%DEPLOYMENT_TARGET%\site\wwwroot
 
-:: 3. Remove app_offline.htm after deployment
-echo Removing app_offline.htm
-del "%DEPLOYMENT_TARGET%\app_offline.htm"
+REM Cleanup old files if necessary
+if exist "%TARGET_PATH%\Data" rmdir /s /q "%TARGET_PATH%\Data"
 
-:: Testing if works
+REM Copy new files
+xcopy /s /e /i "%ZIP_PATH%" "%TARGET_PATH%"
+
+REM Handle any custom file exclusions
+REM Example: Removing unwanted files
+del /q "%TARGET_PATH%\unwanted_file.txt"
